1c8d52a000b4f73f3531de03f1ee8392d6f5a76f
David Rojo <rojo@rojorevolution.com>
2021-04-03 20:41:22 -0500

    changing the plan to show logo in the header after certain scroll position - working on implementing scroll to functionality - set up event delegation

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,28 +1,42 @@
 
+const logoDiv = document.getElementById("logo")
 const logoIcon = document.getElementById("logoIcon")
 const logoText = document.getElementById("logoText")
+const navItems = document.getElementById("mainNav")
+
+
 let topPosition = 175;
 let maxWidth = 425;
+let widthPercent = 100;
+let scrollPos = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    const findPos = (obj) => {
+        let curtop = 0;
+        if (obj.offsetParent) {
+            do {
+                curtop += obj.offsetTop;
+            } while (obj = obj.offsetParent);
+            return [curtop];
+        }
+    }
+
+    navItems.addEventListener('click', (event) => {
+        event.preventDefault();
+        console.log("click")
+        let navItem = event.target.parentElement.id;
+        console.log(navItem)
+    })
+
     window.addEventListener("scroll", () => {
 
-        if (window.scrollY <= 300) {
+        if (window.scrollY > 150) {
             console.log("Scrolling - MaxWidth = ", maxWidth)
-            maxWidth -= 25;
-            // hideLogo();
-            logoText.classList.add('disp-none');
-            // logoIcon.classList.add('logoScroll');
-            // logoIcon.style.top = "20px";
-            // logoIcon.style.maxWidth = `${maxWidth}px`;
+
         }
     })
 
-    // const hideLogo = () => {
-    //     logoText.className.add('disp-none');
-
-    // }
 
 
 });
