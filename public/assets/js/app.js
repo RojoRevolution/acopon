d0e1e72b6fdfc05d8d8ff75eee03f9b95e48b246
David Rojo <rojo@rojorevolution.com>
2021-04-09 12:18:19 -0600

    set up event delegation for nav based on className, using same className for section IDs

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,16 +1,28 @@
-
-const logoDiv = document.getElementById("logo")
-const logoIcon = document.getElementById("logoIcon")
-const logoText = document.getElementById("logoText")
-const navItems = document.getElementById("mainNav")
-
-
-let topPosition = 175;
-let maxWidth = 425;
-let widthPercent = 100;
-let scrollPos = 0;
-
 document.addEventListener("DOMContentLoaded", () => {
+
+    const logoDiv = document.getElementById("logo")
+    const logoIcon = document.getElementById("logoIcon")
+    const logoText = document.getElementById("logoText")
+    const navItems = document.getElementById("mainNav")
+
+    // Navigation Elements
+    const homeNav = document.querySelector(".home")
+    const aboutNav = document.querySelector(".about")
+    const locationNav = document.querySelector(".location")
+    const onTapNav = document.querySelector(".onTap")
+    const OrderNav = document.querySelector(".order")
+
+    // Content Elements
+    const introSection = document.getElementById("introSection")
+    const aboutSection = document.getElementById("aboutSection")
+    const locationSection = document.getElementById("locationSection")
+    const onTapSection = document.getElementById("onTapSection")
+
+    // let topPosition = 175;
+    // let maxWidth = 425;
+    // let widthPercent = 100;
+    // let scrollPos = 0;
+
 
     const findPos = (obj) => {
         let curtop = 0;
@@ -22,20 +34,27 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    // aboutNav.addEventListener('click', () => {
+    //     // Scroll to the y-pos of the portfolioSection
+    //     console.log("CLICK ABOUT NAV")
+    //     window.scrollTo(0, findPos(aboutSection))
+    // })
+
     navItems.addEventListener('click', (event) => {
         event.preventDefault();
-        console.log("click")
-        let navItem = event.target.parentElement.id;
-        console.log(navItem)
+        let navItem = event.target.parentElement.className;
+        console.log("click", navItem)
+        // console.log(navItem)
+        // window.scrollTo(0, findPos(navItem))
+
     })
 
-    window.addEventListener("scroll", () => {
+    // window.addEventListener("scroll", () => {
+    //     if (window.scrollY > 150) {
+    //         console.log("Scrolling - MaxWidth = ", maxWidth)
 
-        if (window.scrollY > 150) {
-            console.log("Scrolling - MaxWidth = ", maxWidth)
-
-        }
-    })
+    //     }
+    // })
 
 
 
