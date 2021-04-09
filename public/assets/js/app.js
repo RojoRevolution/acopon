045159581fc1d1d814e9fdb32ce20e04d426ed80
David Rojo <rojo@rojorevolution.com>
2021-04-09 13:07:13 -0600

    added smooth scrolling CSS - got the scrolling working

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,13 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const logoIcon = document.getElementById("logoIcon")
     const logoText = document.getElementById("logoText")
     const navItems = document.getElementById("mainNav")
+    const headerLogoEl = document.getElementById("headerLogo")
 
     // Navigation Elements
-    const homeNav = document.querySelector(".home")
-    const aboutNav = document.querySelector(".about")
-    const locationNav = document.querySelector(".location")
-    const onTapNav = document.querySelector(".onTap")
-    const OrderNav = document.querySelector(".order")
+
 
     // Content Elements
     const home = document.getElementById("home")
@@ -18,39 +15,29 @@ document.addEventListener("DOMContentLoaded", () => {
     const location = document.getElementById("location")
     const onTap = document.getElementById("onTap")
 
-    // let topPosition = 175;
-    // let maxWidth = 425;
-    // let widthPercent = 100;
-    // let scrollPos = 0;
 
-
+    // Find Scroll position
     const findPos = (obj) => {
         let curtop = 0;
         if (obj.offsetParent) {
             do {
                 curtop += obj.offsetTop;
             } while (obj = obj.offsetParent);
+            console.log("Y Position: ", curtop)
             return [curtop];
         }
     }
 
-    // aboutNav.addEventListener('click', () => {
-    //     // Scroll to the y-pos of the portfolioSection
-    //     console.log("CLICK ABOUT NAV")
-    //     window.scrollTo(0, findPos(aboutSection))
-    // })
-
+    // Event listener for all nav items using event delegation
     navItems.addEventListener('click', (event) => {
-        event.preventDefault();
         let navItem = event.target.parentElement.className;
-        console.log("click", navItem)
-        // console.log(navItem)
         switch (navItem) {
             case "home":
                 window.scrollTo(0, findPos(home))
                 break;
             case "about":
                 window.scrollTo(0, findPos(about))
+                // window.scrollTo(0, 500)
                 break;
             case "location":
                 window.scrollTo(0, findPos(location))
@@ -59,17 +46,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 window.scrollTo(0, findPos(onTap))
                 break;
         }
-
-        // window.scrollTo(0, findPos(navItem))
-
     })
 
-    // window.addEventListener("scroll", () => {
-    //     if (window.scrollY > 150) {
-    //         console.log("Scrolling - MaxWidth = ", maxWidth)
-
-    //     }
-    // })
+    if (window.scrollY >= 400) {
+        headerLogoEl.classList.toggle("hide")
+    }
 
 
 
